fa7e8cc7c939a13f0096a685a0be7910766725da
Michael Kulabuhov <progger.ru@gmail.com>
2012-03-01 00:52:29 +0700

    Простое рисование

--- web/main.js
+++ web/main.js
@@ -1,3 +1,39 @@
-function main() {
+function draw_onMouseDown(e)
+{
+    if (e.which !== 1) return;
+    this.state = true;
+    var ns = this.getAttribute("xmlns");
+    var path = document.createElementNS(ns, "path");
+    path.setAttribute("d", "M" + e.pageX + "," + e.pageY);
+    path.setAttribute("stroke-width", "3");
+    path.setAttribute("stroke", "black");
+    path.setAttribute("fill", "none");
+    this.path = path;
+    this.appendChild(path);
+};
 
+function draw_onMouseUp(e)
+{
+    if (e.which !== 1) return;
+    this.state = false;
 }
+
+function draw_onMouseMove(e)
+{
+    if (!this.state) return;
+    var path = this.path;
+    var d = path.getAttribute("d");
+    d += "L" + e.pageX + "," + e.pageY;
+    path.setAttribute("d", d);
+}
+
+function main()
+{
+    var draw = document.getElementById("drawElement");
+    draw.state = false;
+    draw.onmousedown = draw_onMouseDown;
+    draw.onmouseup = draw_onMouseUp;
+    draw.onmousemove = draw_onMouseMove;
+};
+
+window.onload = main;
